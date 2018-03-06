2ce374cb354072e497d1fff651878c59fbaa678d
Simone Vuotto <simov8@gmail.com>
2018-03-06 13:46:45 +0100

    Move backend url in proxy conf

--- src/app/jwt.interceptor.js.ts
+++ src/app/jwt.interceptor.js.ts
@@ -5,14 +5,12 @@ import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-    const url = 'http://localhost:8080/';
     // add authorization header with jwt token if available
     let authorization = localStorage.getItem('currentUser');
     if (authorization == null) {
       authorization = '';
     }
     request = request.clone({
-      url: url + request.url,
       setHeaders: {
         Authorization: authorization
       }
